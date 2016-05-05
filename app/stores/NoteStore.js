b342830488766781b34cb35edf4312936912ad2f
Silin Na <n1207n@gmail.com>
2016-05-04 20:30:56 -0700

    fix: Removed the parentheses on NoteStore class declaration

--- app/stores/NoteStore.js
+++ app/stores/NoteStore.js
@@ -2,7 +2,7 @@ import uuid from 'node-uuid';
 import alt from '../libs/alt';
 import NoteActions from '../actions/NoteActions';
 
-class NoteStore() {
+class NoteStore {
   constructor() {
     this.bindActions(NoteActions);
 
