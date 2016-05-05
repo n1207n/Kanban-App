8bf5bc0aa2305e6ec94d879b448ec79990a4b3ba
Silin Na <n1207n@gmail.com>
2016-05-04 21:27:40 -0700

    feat: Integrated Lane component and data layer to App component

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -1,41 +1,28 @@
 import React from 'react';
 
-import Notes from './Notes.jsx';
+import Lanes from './Lanes.jsx';
 
 import AltContainer from 'alt-container';
-import NoteActions from '../actions/NoteActions';
-import NoteStore from '../stores/NoteStore';
+import LaneActions from '../actions/LaneActions';
+import LaneStore from '../stores/LaneStore';
 
 export default class App extends React.Component {
   render () {
     return (
       <div>
-        <button className="add-note" onClick={this.addNote}>+</button>
+        <button className="add-lane" onClick={this.addLane}>+</button>
         <AltContainer
-          stores={[NoteStore]}
+          stores={[LaneStore]}
           inject={{
-            notes: () => NoteStore.getState().notes
+            lanes: () => LaneStore.getState().lanes
           }}>
-          <Notes onEdit={this.editNote} onDelete={this.deleteNote} />
+          <Lanes />
         </AltContainer>
       </div>
     )
   }
 
-  addNote = () => {
-    NoteActions.create({task: 'New Task'});
-  }
-
-  editNote = (id, task) => {
-    if(!task.trim()) {
-      return;
-    }
-
-    NoteActions.update({id, task});
-  }
-
-  deleteNote = (id, e) => {
-    e.stopPropagation();
-    NoteActions.delete(id);
+  addLane() {
+    LaneActions.create({name: 'New Lane'});
   }
 }
---
+++ app/components/Lanes.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default class Lanes extends React.Component {
+  render() {
+    return (
+      <div className="lanes">
+
+      </div>
+    )
+  }
+}
