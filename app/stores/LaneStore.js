f219eebeb996cc8a73c8461054de4b8fc530fb7b
Silin Na <n1207n@gmail.com>
2016-05-04 21:22:48 -0700

    feat: Created Action and Store for Lane

---
+++ app/stores/LaneStore.js
@@ -0,0 +1,26 @@
+import uuid from 'node-uuid';
+import alt from '../libs/alt';
+
+import LaneActions from '../actions/LaneActions';
+
+class LaneStore {
+  constructor() {
+    this.bindActions(LaneActions);
+
+    this.lanes = [];
+  }
+
+  create(lane) {
+    const lanes = this.lanes;
+
+    lane.id = uuid.v4();
+
+    lane.notes = lane.notes || [];
+
+    this.setState({
+      lanes: lanes.concat(lane)
+    });
+  }
+}
+
+export default alt.createStore(LaneStore, 'LaneStore');
