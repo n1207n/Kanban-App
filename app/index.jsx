49ca4be10f70bf8f456716f4608b9ad527de0507
Silin Na <n1207n@gmail.com>
2016-05-04 20:43:06 -0700

    feat:Added localStorage API and Alt Store for persisting the entire app state

--- app/index.jsx
+++ app/index.jsx
@@ -2,6 +2,13 @@ import './scss/main.scss';
 
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+import alt from './libs/alt';
+import storage from './libs/storage';
+import persist from './libs/persist';
+
 import App from './components/App.jsx';
+
+persist(alt, storage, 'app');
 
 ReactDOM.render(<App />, document.getElementById('app'));
