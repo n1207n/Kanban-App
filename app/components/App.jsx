4d3675c93aff624d276c277f60102ddb09811592
Silin Na <n1207n@gmail.com>
2016-05-04 21:06:47 -0700

    feat: Added AltContainer to wrap App component bootstrapping
    + Passing data from Store to Notes by AltContainer

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -2,35 +2,22 @@ import React from 'react';
 
 import Notes from './Notes.jsx';
 
+import AltContainer from 'alt-container';
 import NoteActions from '../actions/NoteActions';
 import NoteStore from '../stores/NoteStore';
 
 export default class App extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.state = NoteStore.getState();
-  }
-
-  componentDidMount() {
-    NoteStore.listen(this.storeChanged);
-  }
-
-  componentWillUnmount() {
-    NoteStore.unlisten(this.storeChanged);
-  }
-
-  storeChanged = (state) => {
-    this.setState(state);
-  }
-
   render () {
-    const notes = this.state.notes;
-
     return (
       <div>
         <button className="add-note" onClick={this.addNote}>+</button>
-        <Notes notes={notes} onEdit={this.editNote} onDelete={this.deleteNote} />
+        <AltContainer
+          stores={[NoteStore]}
+          inject={{
+            notes: () => NoteStore.getState().notes
+          }}>
+          <Notes onEdit={this.editNote} onDelete={this.deleteNote} />
+        </AltContainer>
       </div>
     )
   }
