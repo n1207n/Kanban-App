2b47e22201d69dcca35f73430938bdc9e4b520fa
Silin Na <n1207n@gmail.com>
2016-05-24 15:46:49 -0700

    feat: Added DragSource and DropTarget Annotators to Note Component
    + Wrapped React component with the drag and drop callback wrappers

--- app/components/Note.jsx
+++ app/components/Note.jsx
@@ -1,7 +1,36 @@
 import React from 'react';
 
+import {DragSource, DropTarget} from 'react-dnd';
+import ItemTypes from '../constants/itemTypes';
+
+const noteSource = {
+  beginDrag(props) {
+    console.log('begin dragging note', props);
+
+    return {};
+  }
+};
+
+const noteTarget = {
+  hover(targetProps, monitor) {
+    const sourceProps = monitor.getItem();
+
+    console.log('dragging note', sourceProps, targetProps);
+  }
+};
+
+@DragSource(ItemTypes.NOTE, noteSource, (connect) => ({
+  connectDragSource: connect.dragSource()
+}))
+@DropTarget(ItemTypes.NOTE, noteTarget, (connect) => ({
+  connectDropTarget: connect.dropTarget()
+}))
 export default class Note extends React.Component {
   render() {
-    return <li {...this.props}>{this.props.children}</li>
+    const {connectDragSource, connectDropTarget, id, onMove, ...props} = this.props;
+
+    return connectDragSource(connectDropTarget(
+      <li {...this.props}>{this.props.children}</li>
+    ));
   }
 }
